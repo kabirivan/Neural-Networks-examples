74be9d911841e063d801afbca1914bdaa171737b
Ivan Aguas <aguas.xavier2013a@gmail.com>
2020-05-31 12:12:09 -0500

    Add graph of mean absolute percentage error

--- example3.py
+++ example3.py
@@ -9,6 +9,11 @@ Created on Sun May 31 11:27:18 2020
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.datasets import boston_housing
+
+(x_train, y_train), (x_test, y_test) = boston_housing.load_data(test_split = 0.2, seed = 10)
+
+
 
 model1 = Sequential()
 model1.add(Dense(13,input_dim = 13, kernel_initializer='normal', activation = 'relu'))
@@ -19,6 +24,19 @@ model1.add(Dense(1, kernel_initializer='normal'))
 model1.compile(loss='mse', optimizer = 'adam', metrics=['mean_absolute_percentage_error'])
 
 #print(model.summary())
-
 #from keras.utils import plot_model
 #plot_model(model1, to_file = 'firstModel.png', show_shapes=True)
+
+# Training and evaluation 
+
+x_val =  x_train[300:,]
+y_val = y_train[300:,]
+#model1.fit(x_train,y_train, batch_size=32, epochs=100, validation_data=(x_val, y_val))
+
+
+history1 = model1.fit(x_train,y_train, batch_size=32, epochs = 200, validation_data=(x_val, y_val), verbose = 0)
+import matplotlib.pyplot as plt
+plt.plot(history1.history['mean_absolute_percentage_error'])
+plt.plot(history1.history['val_mean_absolute_percentage_error'])
+
+
